d2ff2ed3034709ce723138427eb4ea400c6d52bd
Ekene Madunagu <ekenemadunagu@gmail.com>
2018-07-26 21:15:16 +0100

    initial commit

---
+++ config/services.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+    'firebase' => [
+    'api_key' => env('FIREBASE_APP_ID'), // Only used for JS integration
+    'auth_domain' => 'AUTH_DOMAIN', // Only used for JS integration
+    'database_url' => env('FIREBASE_DATABASE_URL'),
+    'secret' => env('FIREBASE_DATABSE_SECRET'),
+    'storage_bucket' => env('FIREBASE_STORAGE_BUCKET'), // Only used for JS integration
+],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+];
