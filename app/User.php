95c54b75fc4622a37d527b03102301100d3b2b3b
Ekene Madunagu <ekenemadunagu@gmail.com>
2018-07-27 14:13:34 +0100

    created blog models and base relationship

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Mpociot\Firebase\SyncsWithFirebase;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SyncsWithFirebase;
 
     /**
      * The attributes that are mass assignable.
