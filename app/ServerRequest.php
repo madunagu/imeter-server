0f4ce5863816610ed5b8fbb6218a6f7e9565d9e3
= <ekenemadunagu@gmail.com>
2018-10-12 22:13:27 +0100

    protocol refinement

--- app/ServerRequest.php
+++ app/ServerRequest.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ServerRequest extends Model
 {
     #protocol for most communication
-    public static $requestTypeToggleConnection = '1';
+    public static $requestTypeToggleConnection = '2';
     public static $requestKeyToggleConnection = 'CD';
-    public static $requestTypePowerTime = '2';
+    public static $requestTypePowerTime = '1';
     public static $requestKeyPowerTime = 'PT';
     public static $requestKeyEnergyBudget = 'EB';
     public static $requestTypeEnergyBudget = '3';
+    public static $requestKeyIOT = '';
+    public static $requestTypeIOT = '4';
 }
